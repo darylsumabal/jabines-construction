b631d701bc71b7774f9ac598831a3da5e2537861
Legendaryl <darylsumabal123@gmail.com>
2026-09-01 09:42:43 +0800

    ok

--- app/Filament/Resources/Suppliers/Tables/SuppliersTable.php
+++ app/Filament/Resources/Suppliers/Tables/SuppliersTable.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Suppliers\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Actions\ForceDeleteBulkAction;
@@ -35,6 +36,7 @@ class SuppliersTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make()
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
